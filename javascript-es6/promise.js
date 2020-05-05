87b41c7507f9a4540461a8de1eb978a382320483
Alok Tripathi <gnsalok@gmail.com>
2020-05-05 15:23:47 +0530

    working with promises

--- javascript-es6/promise.js
+++ javascript-es6/promise.js
@@ -1,2 +1,18 @@
-const promise = new Promise();
+promise = new Promise((resolve, reject) => {
+  reject();
+});
 
+console.log(promise);
+
+//call when it resolve
+promise
+  .then(() => console.log("Finally resolve!!!"))
+  .then(() => {
+    console.log("Im runing.");
+  })
+  .catch(() => {
+    console.log("Something wrong!!");
+  });
+
+//called when promise reject
+// promise.catch();
